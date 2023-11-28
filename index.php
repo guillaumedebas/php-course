67b12ca147a953084ee86070f72169ca9eeb709d
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-11-28 23:53:19 +0100

    function example

--- index.php
+++ index.php
@@ -234,6 +234,13 @@
         // strlen pour calculer la longueur d'une chaîne de caractères ;
         // str_replace pour rechercher et remplacer une chaîne de caractères ;
         // sprintf pour formater une chaîne de caractères.
+
+        //exemples
+        $recipe = 'Etape 1 : des flageolets ! Etape 2 : de la saucisse toulousaine';
+        $length = strlen($recipe);
+
+
+        echo 'La phrase ci-dessous comporte ' . $length . ' caractères :' . PHP_EOL . $recipe;
         ?>
 
 
