95371f5fedac9f7a84cb3e6d21559fc8bf1a2899
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-11-28 23:51:26 +0100

    functions part1

--- index.php
+++ index.php
@@ -218,6 +218,22 @@
         $positionAnna = array_search('Anna Joud-Debas', $usersTest);
         echo '"Anna" se trouve en position ' . $positionAnna . PHP_EOL;
         echo '<br>';
+
+        //Les fonctions
+        // 
+        // str_replace pour rechercher et remplacer des mots dans une variable ;
+        // move_uploaded_file pour envoyer un fichier sur un serveur ;
+        // imagecreate pour créer des images miniatures (aussi appelées "thumbnails") ;
+        // mail pour envoyer un mail avec PHP (très pratique pour faire une newsletter) ;
+        // de nombreuses options pour modifier des images, y écrire du texte, tracer des lignes, des rectangles, etc. ;
+        // crypt pour chiffrer des mots de passe ;
+        // date  pour renvoyer l'heure, la date, etc.
+
+        // Manipulez du texte avec les fonctions
+        //
+        // strlen pour calculer la longueur d'une chaîne de caractères ;
+        // str_replace pour rechercher et remplacer une chaîne de caractères ;
+        // sprintf pour formater une chaîne de caractères.
         ?>
 
 
