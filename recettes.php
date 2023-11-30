60563b6b9fe540b3ea8bc90d0b197eaa4ee743b3
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-12-01 00:03:27 +0100

    include concept

--- recettes.php
+++ recettes.php
@@ -1,98 +1,31 @@
-<?php
-
-$users = [
-    [
-        'full_name' => 'Mickaël Andrieu',
-        'email' => 'mickael.andrieu@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Mathieu Nebra',
-        'email' => 'mathieu.nebra@exemple.com',
-        'age' => 34,
-    ],
-    [
-        'full_name' => 'Laurène Castor',
-        'email' => 'laurene.castor@exemple.com',
-        'age' => 28,
-    ],
-];
-
-$recipes = [
-    [
-        'title' => 'Cassoulet',
-        'recipe' => '',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Couscous',
-        'recipe' => '',
-        'author' => 'mickael.andrieu@exemple.com',
-        'is_enabled' => false,
-    ],
-    [
-        'title' => 'Escalope milanaise',
-        'recipe' => '',
-        'author' => 'mathieu.nebra@exemple.com',
-        'is_enabled' => true,
-    ],
-    [
-        'title' => 'Salade Romaine',
-        'recipe' => '',
-        'author' => 'laurene.castor@exemple.com',
-        'is_enabled' => false,
-    ],
-];
-
-function displayAuthor(string $authorEmail, array $users) : string
-{
-    for ($i = 0; $i < count($users); $i++) {
-        $author = $users[$i];
-        if ($authorEmail === $author['email']) {
-            return $author['full_name'] . '(' . $author['age'] . ' ans)';
-        }
-    }
-}
-
-function isValidRecipe(array $recipe) : bool
-{
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-function getRecipes(array $recipes) : array
-{
-    $valid_recipes = [];
-
-    foreach($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $valid_recipes[] = $recipe;
-        }
-    }
-
-    return $valid_recipes;
-}
-
-?>
+<!-- index.php -->
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Recettes de cuisine</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Page d'accueil</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
         rel="stylesheet"
     >
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
     <div class="container">
-        <h1>Liste des recettes de cuisine</h1>
 
+    <?php include_once('includes/header.php'); ?>
+        <h1>Site de recettes</h1>
+
+        <!-- inclusion des variables et fonctions -->
+        <?php
+            include_once('includes/variables.php');
+            include_once('includes/functions.php');
+        ?>
+
+        <!-- inclusion de l'entête du site -->
+        <?php include_once('includes/header.php'); ?>
+        
         <?php foreach(getRecipes($recipes) as $recipe) : ?>
             <article>
                 <h3><?php echo $recipe['title']; ?></h3>
@@ -100,6 +33,9 @@ function getRecipes(array $recipes) : array
                 <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
             </article>
         <?php endforeach ?>
-    </div>   
+    </div>
+
+    <!-- inclusion du bas de page du site -->
+    <?php include_once('includes/footer.php'); ?>
 </body>
 </html>
