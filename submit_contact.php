7b20250a7a4d3dc020a5ba20a4402127b20ccdb7
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-12-02 23:56:24 +0100

    secure post reception

--- submit_contact.php
+++ submit_contact.php
@@ -1,39 +1,41 @@
 <?php
 
-if((!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_GET['message']) || empty($_GET['message']))) {
-    echo "Erreur d'informations lors de l'affichage du formulaire";
-
+// submit_contact.php
+if (!isset($_POST['email']) || !isset($_POST['message']))
+{
+	echo('Il faut un email et un message pour soumettre le formulaire.');
     return;
-}
+}	
+
+$email = $_POST['email'];
+$message = $_POST['message'];
 
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Submit Contact</title>
-</head>
-     <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
-</head>
-<body class="d-flex flex-column min-vh-100">
-    <div class="container">
-
-    <?php include_once('includes/header.php'); ?>
-    <h1>Message reçu</h1>
-        <div class="card">
-            <article class="card-body">
-                <h2 class="card-title">Rappel de vos informations</h2>
-                <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?> </p>
-                <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?> </p>
-                   </article>
-    </div>
-
-    <!-- inclusion du bas de page du site -->
-    <?php include_once('includes/footer.php'); ?>
-</body>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Site de Recettes - Demande de contact reçue</title>
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+            rel="stylesheet"
+        >
+    </head>
+    <body>
+        <div class="container">
+    
+        <?php include_once('header.php'); ?>
+            <h1>Message bien reçu !</h1>
+            
+            <div class="card">
+                
+                <div class="card-body">
+                    <h5 class="card-title">Rappel de vos informations</h5>
+                    <p class="card-text"><b>Email</b> : <?php echo($email); ?></p>
+                    <p class="card-text"><b>Message</b> : <?php echo strip_tags($message); ?></p>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
