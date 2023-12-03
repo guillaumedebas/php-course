eb9588ce2fba1f845d44b2e5c73119977c111884
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-12-03 23:07:05 +0100

    Upload files system

--- submit_contact.php
+++ submit_contact.php
@@ -1,41 +1,56 @@
 <?php
 
 // submit_contact.php
-if (!isset($_POST['email']) || !isset($_POST['message']))
-{
-	echo('Il faut un email et un message pour soumettre le formulaire.');
+if (!isset($_POST['email']) || !isset($_POST['message'])) {
+    echo ('Il faut un email et un message pour soumettre le formulaire.');
     return;
-}	
+}
 
 $email = $_POST['email'];
 $message = $_POST['message'];
+
+// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0) {
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['screenshot']['size'] <= 1000000) {
+        $fileInfo = pathinfo($_FILES['screenshot']['name']);
+        $extension = $fileInfo['extension'];
+        $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+        if (in_array($extension, $allowedExtensions)) {
+            // On peut valider le fichier et le stocker définitivement
+            move_uploaded_file($_FILES['screenshot']['tmp_name'], 'uploads/' . basename($_FILES['screenshot']['name']));
+            echo "L'envoi a bien été effectué !";
+        }
+    }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Site de Recettes - Demande de contact reçue</title>
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-            rel="stylesheet"
-        >
-    </head>
-    <body>
-        <div class="container">
-    
+
+<head>
+    <meta charset="UTF-8">
+    <title>Site de Recettes - Demande de contact reçue</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+
         <?php include_once('includes/header.php'); ?>
-            <h1>Message bien reçu !</h1>
-            
-            <div class="card">
-                
-                <div class="card-body">
-                    <h5 class="card-title">Rappel de vos informations</h5>
-                    <p class="card-text"><b>Email</b> : <?php echo($email); ?></p>
-                    <p class="card-text"><b>Message</b> : <?php echo strip_tags($message); ?></p>
-                </div>
+        <h1>Message bien reçu !</h1>
+
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Rappel de vos informations</h5>
+                <p class="card-text"><b>Email</b> : <?php echo ($email); ?></p>
+                <!-- Pour éviter la faille XSS, il suffit d'appliquer la fonction htmlspecialchars  ou  strip_tags sur tous les textes envoyés par vos visiteurs que vous afficherez. -->
+                <p class="card-text"><b>Message</b> : <?php echo strip_tags($message); ?></p>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
