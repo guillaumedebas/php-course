65cbc9d3442aaa0d245f2b17f139dd6aa68b55bc
Guillaume Debas <guillaumedebas@hotmail.fr>
2023-12-11 23:58:56 +0100

    cookie system add

--- includes/login.php
+++ includes/login.php
@@ -4,15 +4,14 @@
         foreach ($users as $user) {
             if ($user['email'] == $_POST['login'] && $user['password'] == $_POST['password']) {
                 if (isset($_POST['stayLoggedIn']) && $_POST['stayLoggedIn'] == 'on') {
-                    setcookie(
+                     setcookie(
                         'LOGGED_USER',
                         $user['email'],
-                        time() + 1 * 24 * 3600,
-                        [
-                            'expires' => time() + 1 * 24 * 3600,
-                            'secure' => true,
-                            'httponly' => true,
-                        ]
+                        time() - 86400, // Expire dans 1 jour
+                        '/', // Chemin d'accès au cookie
+                        '', // Domaine, laisser vide si non nécessaire
+                        isset($_SERVER["HTTPS"]), // Sécurisé si HTTPS est activé
+                        true  // HttpOnly
                     );
                     break;
                 } else {
@@ -32,9 +31,9 @@
     }
   }
 
- if (!isset($_SESSION['LOGGED_USER'])) {
+ if (!isset($_SESSION['LOGGED_USER']) || !isset($_COOKIE['LOGGED_USER'])) {
         include_once('connexion_form.php');
-    } elseif (!$_SESSION['LOGGED_USER']) {
+    } elseif (!$_SESSION['LOGGED_USER'] || !$_COOKIE['LOGGED_USER']) {
         echo "Erreur de Login ou de Password";
         include_once('connexion_form.php');
     }
