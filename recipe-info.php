74fe1eceb7b0ed1092f5d816add6952aaf32c393
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-01-24 23:46:24 +0100

    comments system added

--- recipe-info.php
+++ recipe-info.php
@@ -13,11 +13,22 @@ $retrieveRecipeStatement->execute([
 ]);
 $recipe = $retrieveRecipeStatement->fetch(PDO::FETCH_ASSOC);
 
+$retrieveCommentStatement = $mysqlClient->prepare('SELECT u.full_name, c.comment FROM recipes r INNER JOIN comments c ON c.recipe_id = r.recipe_id INNER JOIN users u ON u.user_id = c.user_id WHERE r.recipe_id = :id');
+$retrieveCommentStatement->execute([
+    'id' => (int)$getData['id'],
+]);
+$comments = [];
+while ($row = $retrieveCommentStatement->fetch(PDO::FETCH_ASSOC)) {
+    $comments[] = $row;
+}
+print_r($comments);
+
+
 ?>
 
 <!-- create-recipes-form.php -->
 <!DOCTYPE html>
-<html>
+<html> 
 
 <head>
     <meta charset="UTF-8">
@@ -29,25 +40,29 @@ $recipe = $retrieveRecipeStatement->fetch(PDO::FETCH_ASSOC);
 
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
-     <?php include_once 'includes/welcome_message.php'; ?>
+        <?php include_once 'includes/welcome_message.php'; ?>
         <?php include_once('includes/login.php'); ?>
 
         <?php if (isset($loggedUser)) :
         ?>
             <?php include_once('includes/header.php'); ?>
 
-        <article>
-            <h3><?php echo $recipe['title']; ?></h3>
-            <div><?php echo $recipe['recipe']; ?></div>
-            <i><?php echo $recipe['author']; ?></i>
-            <ul class="list-group list-group-horizontal">
-                <li class="list-group-item"><a class="link-warning" href="update-recipe.php?id=<?php echo ($recipe['recipe_id']); ?>">Editer la recette</a></li>
-                <li class="list-group-item"><a class="link-danger" href="delete-recipe.php?id=<?php echo ($recipe['recipe_id']); ?>">Supprimer la recette</a></li>
-            </ul>
-        </article>
+            <article>
+                <h3><?php echo $recipe['title']; ?></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo $recipe['author']; ?></i>
+                <?php foreach ($comments as $comment) : ?>
+                    <div><?php echo htmlspecialchars($comment['full_name']); ?>: <?php echo htmlspecialchars($comment['comment']); ?></div>
+                <?php endforeach; ?>
+
+                <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item"><a class="link-warning" href="update-recipe.php?id=<?php echo ($recipe['recipe_id']); ?>">Editer la recette</a></li>
+                    <li class="list-group-item"><a class="link-danger" href="delete-recipe.php?id=<?php echo ($recipe['recipe_id']); ?>">Supprimer la recette</a></li>
+                </ul>
+            </article>
     </div>
 <?php endif; ?>
-    <?php include_once('includes/footer.php'); ?>
+<?php include_once('includes/footer.php'); ?>
 </body>
 
 </html>
